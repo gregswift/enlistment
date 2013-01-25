c37f59aabdddaaa80c842af50e3b13840c25ab75
Greg Swift <greg.swift@rackspace.com>
2013-01-25 15:15:14 -0600

    re-implment with Restless and SQLAlchemy

--- wsgi/hire.py
+++ wsgi/hire.py
@@ -1,130 +1,108 @@
 #!/usr/bin/python
 
+## Imports
+# base
 from flask import Flask, request, render_template, redirect, url_for, flash
+#login
+from flask.ext.login import current_user, login_user, LoginManager, UserMixin
+# restless
+from flask.ext.restless import APIManager
+# sqlalchemy
+from flask.ext.sqlalchemy import SQLAlchemy
+# wtfroms
+from flask.ext.wtf import PasswordField, SubmitField, TextField, Form
 
+## Initialize DB
+DATABASE = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                        'hire.sqlite')
+
+## Setup app
 app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['TESTING'] = True
+app.config['SECRET_KEY'] = os.urandom(24)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///%s' % DATABASE
 
-
-## Login
-from flask.ext.login import (LoginManager, current_user, login_required,
-                            login_user, logout_user, UserMixin, AnonymousUser,
-                            confirm_login, fresh_login_required)
-
-class User(UserMixin):
-    def __init__(self, name, id, active=True):
-        self.name = name
-        self.id = id
-        self.active = active
-    
-    def is_active(self):
-        return self.active
-
-
-class Anonymous(AnonymousUser):
-    name = u"Anonymous"
-
-
-USERS = {
-    1: User(u"greg5320", 1),
-    2: User(u"josh.conant", 2),
-    3: User(u"jesse.gonzalez", 3),
-    4: User(u"jon.q.public", 4, False),
-}
-
-USER_NAMES = dict((u.name, u) for u in USERS.itervalues())
-
-SECRET_KEY = "yeah, not actually a secret"
-DEBUG = True
-
-app.config.from_object(__name__)
-
+## Initialize extensions
+db = SQLAlchemy(app)
+api_manager = APIManager(app, flask_sqlalchemy_db=db)
 login_manager = LoginManager()
-
-login_manager.anonymous_user = Anonymous
-login_manager.login_view = "login"
-login_manager.login_message = u"Please log in to access this page."
-login_manager.refresh_view = "reauth"
-
-@login_manager.user_loader
-def load_user(id):
-    return USERS.get(int(id))
-
-
 login_manager.setup_app(app)
 
-@app.route("/")
+## Setup Database model
+
+# define user model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.Unicode)
+    password = db.Column(db.Unicode)
+
+# define candidate model
+class Candidate(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Unicode)
+
+# define panel model
+class Panel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Unicode)
+    candidateid = db.Column(db.Integer)
+    results = db.Column(db.Integer)
+
+# define vote model
+class Vote(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    panelid = db.Column(db.Integer)
+    userid = db.Column(db.Integer)
+    vote = db.Column(db.Integer)
+
+## Initialize database 
+db.create_all()
+admin = User(username=u'admin', password=u'0qww294e')
+db.session.add(admin)
+db.session.commit()
+
+## Establish Flask_login - required
+@login_manager.user_loader
+def load_user(userid):
+    return User.query.get(userid)
+
+## Create Login Form
+class LoginForm(Form):
+    username = TextField('username')
+    password = PasswordField('password')
+    submit = SubmitField('Login')
+
+## Initialize main page
+@app.route('/', methods=['GET'])
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
-
-@app.route("/secret")
-@fresh_login_required
-def secret():
-    return render_template("secret.html")
-
-
-@app.route("/login", methods=["GET", "POST"])
+## Initialize login form
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == "POST" and "username" in request.form:
-        username = request.form["username"]
-        if username in USER_NAMES:
-            remember = request.form.get("remember", "no") == "yes"
-            if login_user(USER_NAMES[username], remember=remember):
-                flash("Logged in!")
-                return redirect(request.args.get("next") or url_for("index"))
-            else:
-                flash("Sorry, but you could not log in.")
-        else:
-            flash(u"Invalid username.")
-    return render_template("login.html")
+    form = LoginForm()
+    if form.validate_on_submit():
+        #
+        # you would check username and password here...
+        #
+        username, password = form.username.data, form.password.data
+        user = User.query.filter_by(username=username,
+                                    password=password).one()
+        login_user(user)
+        return redirect(url_for('index'))
+    return render_template('login.html', form=form)
 
-
-@app.route("/reauth", methods=["GET", "POST"])
-@login_required
-def reauth():
-    if request.method == "POST":
-        confirm_login()
-        flash(u"Reauthenticated.")
-        return redirect(request.args.get("next") or url_for("index"))
-    return render_template("reauth.html")
-
-
-@app.route("/logout")
-@login_required
-def logout():
-    logout_user()
-    flash("Logged out.")
-    return redirect(url_for("index"))
-
-
-## API
-@app.route("/api")
-def api_root():
-    return
-
-@app.route("/api/candidates")
-def api_candidates():
-    return 'List of ' + url_for('api_candidates')
-
-@app.route('/api/new/panel/<name>')
-def api_new_panel(name):
-    return 'panel {0} added'.format(name)
-
-@app.route('/api/panel/<panelid>/candidate/<candidateid>/<action>')
-def api_new_panel(panelid, candidateid, action):
-    return 'canidate {0} {1}ed to panel {2}'.format(candidateid, action, panelid)
-
-@app.route('/api/panel/<panelid>/panelist/<name>/<action>')
-def api_add_panelist(panelid, name):
-    return 'panelist {0} {1}ed to panel {2}'.format(name, action, panelid)
-
-@app.route('/api/panel/<panelid>/vote/<vote>')
-def api_vote_candidate(panelid, vote):
-    return 'You voted {0} for panel {1}'.format(vote, panelid)
-
-@app.route('/api/panel/<panelid>/results')
-def api_vote_candidate(panelid):
-    return 'You ask for the results for the vote on panel {0}'.format(panelid)
+# Step 8: create the API for User with the authentication guard.
+auth_func = lambda: current_user.is_authenticated()
+api_manager.create_api(User, authentication_required_for=['GET'],
+                       authentication_function=auth_func)
+api_manager.create_api(Candidate, authentication_required_for=['GET','POST'],
+                       authentication_function=auth_func)
+api_manager.create_api(Panel, authentication_required_for=['GET','POST','PATCH'],
+                       authentication_function=auth_func)
+api_manager.create_api(Vote, authentication_required_for=['GET','POST','PATCH'],
+                       authentication_function=auth_func)
 
 if __name__ == "__main__":
     app.run()

dfd3801fa345ada2ec69382322b94b2c6108ad96
Greg Swift <greg.swift@rackspace.com>
2013-01-25 13:55:50 -0600

    fixed import

--- wsgi/hire.py
+++ wsgi/hire.py
@@ -2,7 +2,7 @@
 
 ## Login
 from flask import Flask, request, render_template, redirect, url_for, flash
-from flaskext.login import (LoginManager, current_user, login_required,
+from flask.ext.login import (LoginManager, current_user, login_required,
                             login_user, logout_user, UserMixin, AnonymousUser,
                             confirm_login, fresh_login_required)
 

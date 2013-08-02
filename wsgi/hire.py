b4cb6b9810d0e0f43e6a8d76ea6eaa8da7fa8226
Greg Swift <gregswift@gmail.com>
2013-08-02 10:27:46 -0500

    moved all the database bit to a submodule

--- wsgi/hire.py
+++ wsgi/hire.py
@@ -14,6 +14,9 @@ from flask.ext.sqlalchemy import SQLAlchemy
 # wtfroms
 from flask.ext.wtf import PasswordField, SubmitField, TextField, Form
 
+## Import backend db
+import hire.db as backend
+
 ## Enable logentries for external logging
 from logentries import LogentriesHandler
 import logging
@@ -29,46 +32,13 @@ app.config['SECRET_KEY'] = os.urandom(24)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['OPENSHIFT_POSTGRESQL_DB_URL'] + os.environ['OPENSHIFT_APP_NAME']
 
 ## Initialize extensions
-db = SQLAlchemy(app)
+db = backend.initialize(app)
 api_manager = APIManager(app, flask_sqlalchemy_db=db)
 login_manager = LoginManager()
 login_manager.setup_app(app)
 
-## Setup Database model
-
-# define user model
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.Unicode)
-    password = db.Column(db.Unicode)
-
-# define candidate model
-class Candidate(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Unicode)
-
-# define panel model
-class Panel(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Unicode)
-    candidateid = db.Column(db.Integer)
-    results = db.Column(db.Integer)
-
-# define panelists model
-class Panelist(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    panelid = db.Column(db.Integer)
-    userid = db.Column(db.Integer)
-
-# define vote model
-class Vote(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    panelid = db.Column(db.Integer)
-    userid = db.Column(db.Integer)
-    vote = db.Column(db.Integer)
-
 ## Initialize database 
-db.create_all()
+backend.create_all(db)
 
 ## Establish Flask_login - required
 @login_manager.user_loader
